224ecb849d5e72a839a19c02d7530100993b4cf7
SeptianProject <septianuwoe75@gmail.com>
2025-12-29 22:52:06 +0700

    feat: integrate Prisma client again and set up database schema and setup comon module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.useLogger(logger);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
